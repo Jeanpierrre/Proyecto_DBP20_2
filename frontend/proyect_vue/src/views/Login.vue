e79db597d25eba4bad0c66931aa86ccbc69089ac
Stuart AM <stuart.arteaga@utec.edu.pe>
2022-07-07 02:01:57 -0500

    añadir a la api
    CONEXION CON EL login flata el registro

--- frontend/proyect_vue/src/views/Login.vue
+++ frontend/proyect_vue/src/views/Login.vue
@@ -3,7 +3,7 @@
     <h1>Login</h1>
     <p>Hello {{ name }}</p>
     <form @submit.prevent="login">
-      <input v-model="name" placeholder="Enter your name" /><br />
+      <input v-model="codigo" placeholder="Enter your name" /><br />
       <input
         v-model="password"
         type="password"
@@ -19,9 +19,35 @@ export default {
   name: "Login",
   data() {
     return {
-      name: "",
+      username: "",
       password: "",
     };
+  },
+  methods: {
+    async login() {
+      const url = "http://127.0.0.1:5000/login";
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          codigo: this.username,
+          password: this.password,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("response: ", response);
+      const data = await response.json();
+      console.log("data: ", data);
+      if (data["success"]) {
+        this.$router.push({
+          name: "Todos-Manager",
+          params: {
+            username: data["profile"]["name"],
+          },
+        });
+      }
+    },
   },
 };
 </script>
